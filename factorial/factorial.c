257f50c615c38cb3b6ac13b649ffe11e3b862f97
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-02-15 23:01:39 -0500

    factorial: add buggy example to show usefulness of compiler warnings

---
+++ factorial/factorial.c
@@ -0,0 +1,12 @@
+
+int
+factorial(int n)
+{
+  int res;
+
+  if (n = 0) {
+    res = 1;
+  } else {
+    res = n * factorial(n-1);
+  }
+}
---
+++ factorial/factorial.h
@@ -0,0 +1,7 @@
+
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+int factorial(int n);
+
+#endif
