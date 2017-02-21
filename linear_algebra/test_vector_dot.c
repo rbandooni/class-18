4eb8ce095090e1b1450f829a1723870974fc2128
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-02-20 21:17:29 -0500

    Revert "demonstrate warning when using wrong function name"
    This reverts commit 0b833a93a7c3528eece63c81d2b5f58857adba2e.

--- linear_algebra/test_vector_dot.c
+++ linear_algebra/test_vector_dot.c
@@ -14,7 +14,7 @@ main(int argc, char **argv)
   double x[N] = { 1., 2., 3. };
   double y[N] = { 2., 3., 4. };
 
-  printf("dot product is %g\n", dot_product(x, y));
+  printf("dot product is %g\n", vector_dot(x, y));
 
   return 0;
 }
