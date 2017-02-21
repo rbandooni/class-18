192c9b5505054dff3a49332af0a64716a57ad2d3
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-02-20 21:56:01 -0500

    linalg: shorter way of creating vectors

--- linear_algebra/test_vector_dot.c
+++ linear_algebra/test_vector_dot.c
@@ -12,10 +12,8 @@
 int
 main(int argc, char **argv)
 {
-  double x_vals[3] = { 1., 2., 3. };
-  double y_vals[3] = { 2., 3., 4. };
-  struct vector *x = vector_create_and_set(3, x_vals);
-  struct vector *y = vector_create_and_set(3, y_vals);
+  struct vector *x = vector_create_and_set(3, (double[3]) { 1., 2., 3. });
+  struct vector *y = vector_create_and_set(3, (double[3]) { 2., 3., 4. });
 
   printf("dot product is %g\n", vector_dot(x, y));
   assert(vector_dot(x, y) == 20.);
