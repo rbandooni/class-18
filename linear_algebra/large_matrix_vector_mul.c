ff1bee74d1808ccaaa8fa20fe8ff30b0f9a2439e
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-02-27 21:54:22 -0500

    class10: add setup_test_vectors(), setup_matrix_vectors()
    Still not actually be different than before, but we'll be able to do
    larger dimensions easily by changing N now.

--- linear_algebra/large_matrix_vector_mul.c
+++ linear_algebra/large_matrix_vector_mul.c
@@ -5,6 +5,41 @@
 #include <assert.h>
 
 // ----------------------------------------------------------------------
+// setup_test_vectors
+
+static void
+setup_test_vectors(struct vector *x, struct vector *y_ref)
+{
+  for (int i = 0; i < x->n; i++) {
+    VEC(x, i) = i;
+  }
+
+  for (int i = 0; i < y_ref->n; i++) {
+    VEC(y_ref, i) = i * (i + 1);
+  }
+}
+
+// ----------------------------------------------------------------------
+// setup_test_matrix
+
+static void
+setup_test_matrix(struct matrix *A)
+{
+  // This test matrix is diagonal, which isn't really great,
+  // a more general case would be better.
+  // Then again, at least it makes finding the reference result easy
+  for (int i = 0; i < A->m; i++) {
+    for (int j = 0; j < A->n; j++) {
+      if (i == j) {
+        MAT(A, i, j) = i + 1;
+      } else {
+        MAT(A, i, j) = 0;
+      }
+    }
+  }
+}
+
+// ----------------------------------------------------------------------
 // main
 //
 // test the matrix_vector_mul() function
@@ -12,17 +47,23 @@
 int
 main(int argc, char **argv)
 {
-  struct matrix *A = matrix_create(3, 3);
-  for (int i = 0; i < 3; i++) {
-    MAT(A, i, i) = i + 1;
-  }
-  struct vector *x = vector_create_and_set(3, (double[3]) { 1., 2., 3. });
-  struct vector *y = vector_create(3);
-  struct vector *y_ref = vector_create_and_set(3, (double[3]) { 1., 4., 9. });
+  const int N = 5;
+  
+
+  // set up matrix / vectors 
+  struct matrix *A = matrix_create(N, N);
+  setup_test_matrix(A);
+
+  struct vector *x = vector_create(N);
+  struct vector *y = vector_create(N);
+  struct vector *y_ref = vector_create(N);
+
+  setup_test_vectors(x, y_ref);
 
   matrix_print(A);
   printf("\n");
   
+  // calculate y = Ax
   matrix_vector_mul(A, x, y);
 
   printf("result vector is y = ");
