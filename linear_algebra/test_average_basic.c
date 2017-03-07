1d1a80d03306424ca052a14fd3bc96fc5c6dd76c
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-03-06 22:23:23 -0500

    test_average: add a test_average_basic variant
    This one just uses regular C arrays etc., so it's easier to follow,
    but then it doesn't benefit from things like bounds checking, etc.
    
    # Conflicts:
    #	linear_algebra/Makefile.am

---
+++ linear_algebra/test_average_basic.c
@@ -0,0 +1,49 @@
+
+#include "linear_algebra.h"
+
+#include <math.h>
+#include <stdio.h>
+
+// ----------------------------------------------------------------------
+// calculate vector average (nc -> cc)
+
+void
+vector_average(float *cc, float *nc, int n)
+{
+  for (int i = 0; i < n; i++) {
+    cc[i] = .5 * (nc[i] + nc[i+1]);
+  }
+}
+
+// ----------------------------------------------------------------------
+// main
+
+int
+main(int argc, char **argv)
+{
+  int N = 100;
+  float x_nc[N+1];
+  
+  float dx = 2. * M_PI / N;
+  for (int i = 0; i <= N; i++) {
+    float x = i * dx;
+    x_nc[i] = sin(x) + 1./3. * sin(3.*x);
+  }
+
+  FILE *file = fopen("x_nc.asc", "w");
+  for (int i = 0; i <= N; i++) {
+    float x = i * dx;
+    fprintf(file, "%g %g\n", x, x_nc[i]);
+  }
+  fclose(file);
+
+  float x_cc[N];
+  vector_average(x_cc, x_nc, N);
+
+  file = fopen("x_cc.asc", "w");
+  for (int i = 0; i < N; i++) {
+    float x = (i + .5) * dx;
+    fprintf(file, "%g %g\n", x, x_cc[i]);
+  }
+  fclose(file);
+}
