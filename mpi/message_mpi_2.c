b926c3bfeeda6bd087d33941a0267c8161776545
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-03-27 22:05:19 -0400

    add message_mpi_2 -- with an actual message sent and received

---
+++ mpi/message_mpi_2.c
@@ -0,0 +1,32 @@
+
+#include <mpi.h>
+#include <stdio.h>
+
+// ----------------------------------------------------------------------
+// main
+
+int
+main(int argc, char **argv)
+{
+  int test = 0; 
+
+  MPI_Init(&argc, &argv);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  
+  if (rank == 0) { 
+    test = 99; 
+    MPI_Send(&test, 1, MPI_INT, 3, 123, MPI_COMM_WORLD);
+  } else if (rank == 3) { 
+    MPI_Recv(&test, 1, MPI_INT, 0, 123, MPI_COMM_WORLD,  
+             MPI_STATUS_IGNORE);
+  } 
+
+  printf("Hello %d/%d test = %d\n", rank, size, test);
+
+  MPI_Finalize();
+  return 0;
+}
+  
