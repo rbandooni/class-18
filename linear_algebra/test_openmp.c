e9d0ef8ff6c85bfbaa32631a47dbddc2fd454f96
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-03-27 21:59:45 -0400

    hello_mpi_4.c: to be compared with test_openmp

--- linear_algebra/test_openmp.c
+++ linear_algebra/test_openmp.c
@@ -8,7 +8,11 @@
 int
 main(int argc, char **argv)
 {
+  printf("Hi, just starting\n");
+
 #pragma omp parallel
   printf("Hi, I'm thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+
+  printf("I'm about to be done.\n");
   return 0;
 }
