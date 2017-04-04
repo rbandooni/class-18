7064f7e0c1fa8e061a4fc42e7889ad05ecc2e4b7
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-04-03 22:16:16 -0400

    test_integrate: run in parallel, distribute work
    That is, each proc now only does a subset of the entire loop. There's
    no actual communication yet, each proc prints their own partial sum,
    so to get the total result, one needs to add those partial sums up by
    hand for now.

--- mpi/test_integrate.c
+++ mpi/test_integrate.c
@@ -1,6 +1,10 @@
 
+#include "mpi_debug.h"
+
+#include <mpi.h>
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
 static double
 f(double x)
@@ -11,16 +15,27 @@ f(double x)
 int
 main(int argc, char **argv)
 {
+  MPI_Init(&argc, &argv);
+  
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  
   const int N = 1000;
   double dx = 1. / N;
 
+  assert(N % size == 0);
+  int n = N / size;
+  int ib = rank * n, ie = (rank + 1) * n;
+
   double sum = 0.;
-  for (int i = 0; i < N; i++) {
+  for (int i = ib; i < ie; i++) {
     double x0 = i * dx;
     double x1 = (i+1) * dx;
     sum += .5 * (f(x0) + f(x1)) * dx;
   }
-  printf("sum = %g\n", sum);
+  mprintf("sum = %g\n", sum);
 
+  MPI_Finalize();
   return 0;
 }
