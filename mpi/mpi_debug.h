cf2a4345d132e6600729adeb9f016914c9d4cb6c
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-04-03 22:05:55 -0400

    add mpi_debug.h
    It's not currently used because things work fine, anyway, but it's a
    low-tech way that can be useful for debugging MPI programs.

---
+++ mpi/mpi_debug.h
@@ -0,0 +1,12 @@
+
+#ifndef MPI_DEBUG_H
+#define MPI_DEBUG_H
+
+#include <mpi.h>
+
+#define mprintf(fmt...) do { int __rank; MPI_Comm_rank(MPI_COMM_WORLD, &__rank); { printf("[%d] ", __rank); printf(fmt); } } while(0)
+
+#define MHERE do { int __rank; MPI_Comm_rank(MPI_COMM_WORLD, &__rank); printf("[%d] HERE: in %s() at %s:%d\n", __rank, __FUNCTION__, __FILE__, __LINE__); } while(0)
+
+#endif
+
